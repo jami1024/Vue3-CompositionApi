935b661798978b1ffcfd1a5195c87bdb084da9b2
jijianming <996013797@qq.com>
2022-02-09 20:09:54 +0800

    OptionsAPI的弊端

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import { createApp } from 'vue'
-import App from './01_mixin和extends/App.vue'
+import App from './02_compositionAPI基础/App.vue'
 
 const app = createApp(App);
 app.mount("#app");
 
+app.mixin({
+  data() {
+    return {}
+  },
+  methods: {
 
-// app.mixin({
-//   data() {
-//     return {}
-//   },
-//   methods: {
-
-//   },
-//   created() {
-//     console.log("全局的created生命周期");
-//   }
-// });
+  },
+  created() {
+    console.log("全局的created生命周期");
+  }
+});
