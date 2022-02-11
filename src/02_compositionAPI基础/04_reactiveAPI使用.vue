7c245e196593de1c4661da6fc70f74e68cb3e21a
jijianming <996013797@qq.com>
2022-02-11 13:27:58 +0800

    ref api

--- src/02_compositionAPI基础/04_reactiveAPI使用.vue
+++ src/02_compositionAPI基础/04_reactiveAPI使用.vue
@@ -3,13 +3,13 @@
       <h1>Home page</h1>
       <h2>{{message}}</h2>
       <h2>{{title}}</h2>
-      <h2>当前计数：{{counter}}</h2>
+      <h2>当前计数：{{state.counter}}</h2>
       <button @click="increment">+1</button>
     </div>
 </template>
 
 <script>
-  import { ref } from 'vue'
+  import { reactive } from 'vue'
   export default {
     props: {
       message: {
@@ -20,15 +20,17 @@
 
     setup(props, context) {
 
-      const counter = ref(100)
+      const state = reactive({
+        counter: 100
+      })
       // 局部函数
       const increment = () => {
         console.log('+1')
-        counter.value++
+        state.counter++
       }
       return {
         title: '这是title',
-        counter,
+        state,
         increment
       }
     }
