165c6367de88b8f0f00f9f2645b773e6b145ad77
jijianming <996013797@qq.com>
2022-02-15 00:23:38 +0800

    watchEffect

---
+++ src/04_computed和watch/02_watachEffect.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>{{fullName}}</h3>
+    <button @click="changeFirstName">修改firstName</button>
+    <button @click="changeLastName">修改lastName</button>
+  </div>
+</template>
+
+<script>
+  import { ref, computed, watchEffect } from 'vue'
+  export default {
+    setup() {
+      const firstName = ref('Kobe')
+      const lastName = ref('Bryant')
+      const fullName = computed(() => firstName.value + ' ' +  lastName.value)
+
+      const changeFirstName = () => {
+        firstName.value = 'James'
+      }
+      const changeLastName = () => {
+        lastName.value = 'ssss'
+      }
+      // watchEffect: 自动收集响应式的依赖
+      watchEffect(() => {
+        console.log("firstname:", firstName.value, 'lastname:', lastName.value)
+      })
+      return {
+        firstName,
+        lastName,
+        fullName,
+        changeFirstName,
+        changeLastName
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/04_computed和watch/App.vue
+++ src/04_computed和watch/App.vue
@@ -1,35 +1,22 @@
 <template>
   <div>
-    <h3>{{fullName}}</h3>
-    <button @click="changeFirstName">修改firstName</button>
-    <button @click="changeLastName">修改lastName</button>
+    <h2 ref="title">哈哈哈</h2>
   </div>
 </template>
 
 <script>
-  import { ref, computed, watchEffect } from 'vue'
+  import { ref, watchEffect } from 'vue'
   export default {
     setup() {
-      const firstName = ref('Kobe')
-      const lastName = ref('Bryant')
-      const fullName = computed(() => firstName.value + ' ' +  lastName.value)
-
-      const changeFirstName = () => {
-        firstName.value = 'James'
-      }
-      const changeLastName = () => {
-        lastName.value = 'ssss'
-      }
+      const title = ref(null)
       // watchEffect: 自动收集响应式的依赖
       watchEffect(() => {
-        console.log("firstname:", firstName.value, 'lastname:', lastName.value)
+        console.log('titel', title.value)
+      }, {
+        flush: 'post'
       })
       return {
-        firstName,
-        lastName,
-        fullName,
-        changeFirstName,
-        changeLastName
+        title
       }
     }
   }
