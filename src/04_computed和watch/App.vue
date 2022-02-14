6a8ddd80d8b32e198fba009c3cef707a545c5715
jijianming <996013797@qq.com>
2022-02-14 19:44:59 +0800

    computed

---
+++ src/04_computed和watch/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>{{fullName}}</h3>
+    <button @click="changeName">修改firstName</button>
+  </div>
+</template>
+
+<script>
+  import { ref, computed } from 'vue'
+  export default {
+    setup() {
+      const firstName = ref('Kobe')
+      const lastName = ref('Bryant')
+      const fullName = computed(() => firstName.value + ' ' +  lastName.value)
+
+      const changeName = () => {
+        firstName.value = 'James'
+      }
+      return {
+        firstName,
+        lastName,
+        fullName,
+        changeName
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
