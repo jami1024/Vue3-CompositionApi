b502dc6a597b15b1c776588e91a3d0797a98ea22
jijianming <996013797@qq.com>
2022-02-15 01:00:02 +0800

    watch的基本使用

--- src/04_computed和watch/App.vue
+++ src/04_computed和watch/App.vue
@@ -1,22 +1,41 @@
 <template>
   <div>
-    <h2 ref="title">哈哈哈</h2>
+    <h2 ref="title">{{info.name}}</h2>
+    <button @click="chanageData">修改数据</button>
   </div>
 </template>
 
 <script>
-  import { ref, watchEffect } from 'vue'
+  import { ref, reactive, watchEffect, watch } from 'vue'
   export default {
     setup() {
-      const title = ref(null)
-      // watchEffect: 自动收集响应式的依赖
-      watchEffect(() => {
-        console.log('titel', title.value)
-      }, {
-        flush: 'post'
+      const info = reactive({
+        name: 'why',
+        age: 18
       })
+      // 1.侦听watch时，传入一个getter函数
+      // watch(() => info.name, (newValue, oldValue) => {
+      //   console.log('new', newValue, 'old', oldValue)
+      // })
+      // 2.传入一个可响应式对象：ref对象
+      // 2.1 reactive 对象获取到的new和old本身都是reactive
+      
+      // watch(info, (newValue, oldValue) => {
+      //   console.log('new', newValue, 'old', oldValue)
+      // })
+
+      // 2.2 ref对象获取到的new和old本身式value值
+      const name = ref('ref Test')
+      watch(name, (newValue, oldValue) => {
+        console.log('new', newValue, 'old', oldValue)
+      })
+      const chanageData = () => {
+        name.value = 'zhangsan'
+      }
       return {
-        title
+        chanageData,
+        info,
+        name
       }
     }
   }
